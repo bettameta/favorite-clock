90e81fcab861be1d90688aff3e6910fe8cdb4e84
reese_s <icipherfly@gmail.com>
2020-08-20 10:42:49 -0700

    Adding time features to this clock for Freddy.  Also used urls for images but will keep the img folder with file as back-up.

--- script.js
+++ script.js
@@ -1,35 +1,119 @@
-
-
-var time = new Date().getHours();
-var messageText;
 var noon = 12;
-var evening = 18; // 6PM
-var wakeupTime = 9; // 9AM
+var evening = 6; // 6PM
+var wakeupTime = 7; // 7AM
 var lunchTime = 12; // 12PM
 var partyTime = 17; // 5PM
-var napTime = lunchTime + 2; // 2PM
-var whatTimeIsItJS = document.getElementById("timeEvent");
+var napTime = lunchTime + 3; // 3PM
 
+// NEW FUNCTION & CALLING IT //
+// CODE TO GET STORE REAL TIME //
+var time = new Date().getHours();
+ 
+var updateClock = function() 
+{
+ 
+// the rest of the LOLCat code you wrote up until this step
+// ADD NEW VARIABLES AND FUNCTION CODES HERE //
+
+// MESSAGE TEXT FOR TIME OUTPUT HERE //
+
+var whatTimeIsItJS = document.getElementById('timeEvent');
+var messageText;
+
+// IMAGE FOR TIME OUTPUT HERE //
+var lolcat = document.getElementById('lolcat');
+var image = "https://i.postimg.cc/fLQKk3FH/ozzy-else.jpg";
+
+// START TIMED FUNCTIONS HERE //
 if (time == partyTime){
-
-    messageText = "IZ PARTEE TIME!!";
-} else if (time == napTime) {
-
-    messageText = "IZ NAP TIME...";
-} else if (time == lunchTime) {
 	
-    messageText = "IZ NOM NOM NOM TIME!!";
-} else if (time == wakeupTime) {
-	
-    messageText = "IZ TIME TO GETTUP.";
+    image = "https://i.postimg.cc/JzwWrxxZ/party-time3.jpg";
+    messageText = "IT'S PARTEE TIME!!";
+
+} else if (time == napTime) {	
+
+    image = "https://i.postimg.cc/vZ1dMj5Q/george-nap.jpg";
+    messageText = "IT'S NAP TIME...";
+  
+} else if (time == lunchTime) {	
+    image = "https://i.postimg.cc/kMt1JmpY/henry-lunch.jpg";
+    messageText = "IT'S NOM NOM NOM TIME!!";
+  
+} else if (time == wakeupTime) {	
+    image = "https://i.postimg.cc/7L0hPbSY/harry-wake-up.jpg";
+    messageText = "IT'S TIME TO GET UP.";
+  
 } else if (time < noon) {
-	
+
     messageText = "Good morning!";
+  
+  
 } else if (time > evening) {
-	
+
     messageText = "Good Evening!";
+  
 } else {
-	
+
     messageText = "Good afternoon!";
 }
+
+// MOVED THE BOTTOM OF CODE UP WHEN ADDED "CLOCK TIME KEEPERS"
+
+// TIME TEXT CALL OUT HERE //
 whatTimeIsItJS.innerText = messageText;
+
+// TIME IMAGE CALL OUT HERE //
+lolcat.src = image;
+
+ 
+showCurrentTime();
+ 
+};
+
+// CLOCK TIME KEEPERS //
+var showCurrentTime = function()
+
+{
+    // display the string on the webpage
+    var clock = document.getElementById('clock');
+ 
+    var currentTime = new Date();
+ 
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+ 
+    // Set hours 
+    if (hours >= noon) 
+    { 
+        meridian = "PM"; 
+    }  
+    if (hours > noon) 
+    { 
+        hours = hours - 12; 
+    }
+ 
+    // Set Minutes
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+ 
+    // Set Seconds
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+ 
+    // put together the string that displays the time
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
+ 
+    clock.innerText = clockTime;
+};
+
+
+ 
+updateClock();
+var oneSecond = 1000;
+setInterval( updateClock, oneSecond);
